74fa6917067e85e8ee32b628acbc92841b4241d5
blastpipe <88486744+blastpipe@users.noreply.github.com>
2021-08-29 14:36:50 -0500

    Update Blast hub v0.4.lua

--- Blast hub v0.4.lua
+++ Blast hub v0.4.lua
@@ -31,7 +31,7 @@ Main:addButton("Massplay ( auto syncs )", function()
             obj.Parent = lp.Character
             obj.Remote:FireServer("PlaySong", ID)
             wait(0)
-            for _, obj in next, lp:GetDescendants() do
+            for _, obj in next, workspace:GetDescendants() do
             	if obj:IsA("Sound") then
             		obj.TimePosition = TimePosition or 0
 				end
@@ -215,6 +215,81 @@ Misc:addButton("Suitcase boombox ( must be r6 )", function()
                         t.Parent = plr.Character
             end
         end      
+end)
+Misc:addTextBox("Rejoin dupe", "test", function(new, old, enter)
+local AmountToDupe = tonumber(new)
+
+local plr = game.Players.LocalPlayer
+local char = plr.Character
+local pos = char.HumanoidRootPart.Position
+local http = game:GetService("HttpService")
+
+local file = 'rejoindupe.json'
+writefile(file,http:JSONEncode({
+   ['AmountToDupe'] = AmountToDupe,
+   ['CurrentAmount'] = 0
+}))
+char.HumanoidRootPart.CFrame = CFrame.new(0,999999,0)
+for i,v in pairs(plr.Backpack:GetChildren()) do
+   if v:IsA("Tool") then
+       v.Parent = char
+   end
+end
+wait(.3)
+for i,v in pairs(char:GetChildren()) do
+   if v:IsA("Tool") then
+       v.Parent = workspace
+   end
+end
+
+local dupescript = [[
+   game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()
+   repeat wait() until game.Players.LocalPlayer
+   repeat wait() until game.Players.LocalPlayer.Character
+   repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(%s)
+   local plr = game.Players.LocalPlayer
+   local char = plr.Character
+   local file = "%s"
+   local a = readfile(file)
+   local data = game:GetService("HttpService"):JSONDecode(a)
+   if data['CurrentAmount'] >= data['AmountToDupe'] then
+       wait(0.5)
+       for i,v in pairs(workspace:GetChildren()) do
+           if v:IsA("Tool") then
+               char.Humanoid:EquipTool(v)
+           end
+       end
+       return
+   else
+       wait()
+       char.HumanoidRootPart.CFrame = CFrame.new(0,999999,0)
+       for i,v in pairs(plr.Backpack:GetChildren()) do
+           if v:IsA("Tool") then
+               v.Parent = char
+           end
+       end
+       wait(.3)
+       for i,v in pairs(char:GetChildren()) do
+           if v:IsA("Tool") then
+               v.Parent = workspace
+           end
+       end
+       wait(.1)
+       data['CurrentAmount'] = data['CurrentAmount'] + 1
+       writefile(file,game:GetService("HttpService"):JSONEncode(data))
+   end
+   queue_on_teleport(readfile("%s"))
+   repeat
+   game:GetService("TeleportService"):Teleport(game.PlaceId)    
+   wait(0.5)
+   until nil
+]]
+dupescript = string.format(dupescript,tostring(pos),file,"dupescript.lua")
+
+writefile('dupescript.lua',dupescript)
+queue_on_teleport(dupescript)
+game:GetService("TeleportService"):Teleport(game.PlaceId)
 end)
 TimePos:addButton("Troll Player, sets time pos at a random number", function()
 	    for _, obj in next, Target2.Character:GetDescendants() do
